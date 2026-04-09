20ae377b572a2702fd8358e5789c2d38c78cb54e
MattB <doofymatt@gmail.com>
2026-04-09 11:18:57 -0230

    Added some content to home page and main.py

--- main.py
+++ main.py
@@ -7,3 +7,11 @@ app.config['SESSION_PERMANENT'] = True
 app.config['SESSION_TYPE'] = "filesystem"
 
 Session(app)
+
+shows = {}
+
+@app.route('/')
+def title_page():
+    if "shows" not in session:
+        flash("Welcome to the site, please add a game to get started!!")
+    return render_template("main.html")
