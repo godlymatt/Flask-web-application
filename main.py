3e9c1a94849809faebe11eec1026a9751e908c43
MattB <doofymatt@gmail.com>
2026-04-15 12:10:36 -0230

    Completed the display, and main files up to remove. Started remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session
 from flask_session import Session
+import os
 
 app = Flask(__name__)
 
@@ -21,4 +22,31 @@ def title_page():
 def grabbing():
     return render_template('form.html')
 
+@app.route('/hello', methods=['POST'])
+def adding():
+    showName = request.form['name']
+    favCharacter = request.form['char']
+    summary = request.form['sum']
+    startingYear = request.form['start']
+    poster = request.files['showPic']
+    if poster.filename != '':
+        savePosterName = os.path.join(posterSaveLocation, poster.filename)
+        poster.save(savePosterName)
+    
+    if "shows" in session:
+        session["shows"][showName] = {"Favourite Character":favCharacter, "Summary":summary, "Starting year":startingYear, "poster":poster.filename}
 
+    else:
+        session["shows"] = {}
+        session["shows"][showName] = {"Favourite Character":favCharacter, "Summary":summary, "Starting year":startingYear, "poster":poster.filename}
+
+    flash(f"Show {showName} has been added!")
+    return render_template('form.html')
+
+@app.route('/display')
+def displaying():
+    print(session)
+    return render_template('display.html', shows = session.get("shows", {}), posterLocation = posterSaveLocation)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
